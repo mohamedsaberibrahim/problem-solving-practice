a59c6715484d515732a36a468747964da18024b7
Mohamed Saber <33963542+mohamedsaberibrahim@users.noreply.github.com>
2020-09-29 08:12:43 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

---
+++ leetcode-problems/problems/merge_sorted_array/solution.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int idx = 0;
+        
+        for(int i=m;i<n+m;i++){
+            nums1[i] = nums2[idx];
+            idx++;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
