3d1ad8c0a4022d3d309247f073f461fa26df471d
Mohamed Saber <33963542+mohamedsaberibrahim@users.noreply.github.com>
2020-10-21 05:58:55 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- leetcode-problems/problems/reverse_integer/solution.cpp
+++ leetcode-problems/problems/reverse_integer/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int reverse(int x) {
         long result = 0;
-        while(x != 0)
-        {
-            int curr = x % 10;
-            if ((result * 10 > INT_MAX) || (result * 10 < INT_MIN))
-            {
+        while(x != 0){
+            if((((result* 10) + x%10) > INT_MAX) || (((result* 10) + x%10) < INT_MIN)){
                 return 0;
             }
-            result = (result * 10) + curr;
-            x = x / 10;
+            else{
+                result = (result* 10) + x%10;
+                x = x/10;
+            }
+            
         }
         return result;
     }
