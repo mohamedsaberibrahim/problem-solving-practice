c45395590476ec02c686b398b0fe6e1a8b2c6dc4
Mohamed Saber <33963542+mohamedsaberibrahim@users.noreply.github.com>
2023-02-20 06:14:05 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- leetcode-problems/problems/merge_two_sorted_lists/solution.cpp
+++ leetcode-problems/problems/merge_two_sorted_lists/solution.cpp
@@ -10,28 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* result = new ListNode(0);
-        ListNode* curr = result;
-        while((l1 != nullptr)||(l2 != nullptr)){
-            if(l1 == nullptr){
-                curr->next = l2;
-                return result->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL && list2 == NULL) return list1;
+        ListNode* mergedList = new ListNode(0); 
+        ListNode* currentPointerList1 = list1;
+        ListNode* currentPointerList2 = list2;
+        ListNode* pointerMergedList = mergedList;
+        int currentValList1 = -101;
+        int currentValList2 = -101;
+        while(currentPointerList1 || currentPointerList2) {
+            if (currentPointerList1) {
+                currentValList1 = currentPointerList1->val;
+            } else {
+                currentValList1 = 101;
             }
-            if(l2 == nullptr){
-                curr->next = l1;
-                return result->next;
+            if (currentPointerList2) {
+                currentValList2 = currentPointerList2->val;
+            } else {
+                currentValList2 = 101;
             }
-            if(l1->val < l2->val){
-                curr->next = new ListNode(l1->val);
-                l1 = l1->next;
+            if (currentValList1 >= currentValList2) {
+                pointerMergedList->next = new ListNode(currentValList2);
+                if (currentValList2 != 101) currentPointerList2 = currentPointerList2->next;
             }
-            else{
-                curr->next = new ListNode(l2->val);
-                l2 = l2->next;
+            else if (currentValList1 < currentValList2) {
+                pointerMergedList->next = new ListNode(currentValList1);
+                if (currentValList1 != 101) currentPointerList1 = currentPointerList1->next;
             }
-            curr = curr->next;
+            pointerMergedList = pointerMergedList->next;
         }
-        return result->next;
+        return mergedList->next;
     }
 };
